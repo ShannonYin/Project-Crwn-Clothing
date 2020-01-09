3041cd438229635c88486af529901c478b9b458a
JeanYin <yinxiaohan@outlook.com>
2020-01-09 17:23:03 -0600

    adding HOC with WithSpinner component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
 import {createStructuredSelector} from 'reselect';
 import {selectCartHidden} from '../../redux/cart/cart.selectors';
 import {selectCurrentUser} from '../../redux/user/user.selectors';
